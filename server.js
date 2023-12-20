e8b528b6a2ebca7051bba689f032c0f8ebd846de
Ksiyeong <philip2767@gmail.com>
2023-12-20 20:22:39 +0900

    GET요청 보내는 법

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express();
 app.listen(8080, function () {
     console.log('listening on 8080');
 });
+
+app.get('/pet', function (req, res) {
+    res.send('펫 용품을 쇼핑할 수 있는 페이지 입니다.')
+});
+
+app.get('/beauty', function (req, res) {
+    res.send('뷰티 용품을 쇼핑할 수 있는 페이지 입니다.')
+});
