08b2920fa76e017cc4bd4728e518f106e59cd442
Ksiyeong <philip2767@gmail.com>
2023-12-22 15:52:10 +0900

    write.html 추가

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ app.listen(8080, function () {
     console.log('listening on 8080');
 });
 
-app.get('/pet', function (req, res) {
-    res.send('펫 용품을 쇼핑할 수 있는 페이지 입니다.');
-});
-
-app.get('/beauty', function (req, res) {
-    res.send('뷰티 용품을 쇼핑할 수 있는 페이지 입니다.');
-});
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname + '/write.html');
 });
