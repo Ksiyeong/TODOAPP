f1d8768ac6ae0eb480d76b311f9aabbf41e3a6dc
Ksiyeong <philip2767@gmail.com>
2024-01-03 16:27:23 +0900

    POST /login 구현, home.ctrl.js : output, process 분리

--- prac-login/app/src/routes/home/home.ctrl.js
+++ prac-login/app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+// 임시 데이터
+const users = {
+    ids: ["test0", "test1", "test2"],
+    passwords: ["0000", "1111", "2222"],
+};
+
+const process = {
+    login: (req, res) => {
+        const { id, password } = req.body;
+
+        if (users.ids.includes(id)) {
+            const idx = users.ids.indexOf(id);
+            if (users.passwords[idx] === password) {
+                return res.json({
+                    success: true,
+                    msg: "로그인 성공",
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인 실패",
+        });
+    }
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
