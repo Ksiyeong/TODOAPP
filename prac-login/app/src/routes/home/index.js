f3d78f8a2abf5674c9c49fe6da1ce84da9362602
Ksiyeong <philip2767@gmail.com>
2024-01-14 20:16:17 +0900

    주석 추가

--- prac-login/app/src/routes/home/index.js
+++ prac-login/app/src/routes/home/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
+// 프론트엔드
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
 
+// 백엔드
 router.post("/login", ctrl.process.login);
 router.post("/register", ctrl.process.register);
 router.delete("/users", ctrl.process.deleteUser);
