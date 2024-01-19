ae2d39622de238b2e6f1c747580107f0497fb917
Ksiyeong <philip2767@gmail.com>
2024-01-19 23:01:06 +0900

    User 수정, 삭제 인증 로직 추가

--- prac-login/app/src/routes/home/index.js
+++ prac-login/app/src/routes/home/index.js
@@ -2,8 +2,8 @@
 
 const express = require("express");
 const router = express.Router();
-
 const ctrl = require("./home.ctrl");
+const { verifyAccessTokenAndMatchId } = require("../../middlewares/jwt/jwtUtils");
 
 // 프론트엔드
 router.get("/", ctrl.output.home);
@@ -14,7 +14,7 @@ router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 router.post("/users", ctrl.process.register);
-router.delete("/users", ctrl.process.deleteUser);
-router.patch("/users", ctrl.process.patchUser);
+router.delete("/users", verifyAccessTokenAndMatchId, ctrl.process.deleteUser);
+router.patch("/users", verifyAccessTokenAndMatchId, ctrl.process.patchUser);
 
 module.exports = router;
