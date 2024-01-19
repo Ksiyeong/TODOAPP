f4163758abc5d04fc8fe2889654d6cc39d859572
Ksiyeong <philip2767@gmail.com>
2024-01-19 23:00:32 +0900

    id와 accessToken 매칭 검증 미들웨어 추가

--- prac-login/app/src/middlewares/jwt/jwtUtils.js
+++ prac-login/app/src/middlewares/jwt/jwtUtils.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const jwt = require('jsonwebtoken');
+const CustomError = require('../error/CustomError');
 
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
@@ -9,6 +10,7 @@ function generateAccessToken(id) {
     return jwt.sign(
         {
             id: id,
+            // TODO 권한 관련하여 추가할 것
         },
         JWT_ACCESS_SECRET,
         {
@@ -19,23 +21,7 @@ function generateAccessToken(id) {
     );
 };
 
-function verifyAccessToken(accessToken) {
-    try {
-        return jwt.verify(accessToken, JWT_ACCESS_SECRET);
-        
-    } catch (error) {
-        if (error.name === "TokenExpiredError") {
-            return { success: false, status: 419, msg: "만료된 토큰입니다." };
-        } else if (error.name === "JsonWebTokenError") {
-            return { success: false, status: 401, msg: "유효하지 않는 토큰입니다." };
-        } else {
-            console.log(error);
-            return { success: false, status: 500, msg: error };
-        }
-    }
-};
-
-function generateRefreshToken(id) {// role 추가
+function generateRefreshToken(id) {//TODO role 추가
     return jwt.sign(
         {
             id: id,
@@ -55,19 +41,43 @@ function refreshAccessToken(refreshToken) {
         return generateAccessToken(id);
     } catch (error) {
         if (error.name === "TokenExpiredError") {
-            return { success: false, status: 419, msg: "만료된 토큰입니다." };
+            throw new CustomError(error.name, 419, "JWT419", "만료된 토큰입니다.");
         } else if (error.name === "JsonWebTokenError") {
-            return { success: false, status: 401, msg: "유효하지 않는 토큰입니다." };
+            throw new CustomError(error.name, 401, "JWT401", "유효하지 않는 토큰입니다.");
         } else {
-            console.log(error);
-            return { success: false, status: 500, msg: error };
+            throw error;
         }
     }
 };
+
+function verifyAccessToken(accessToken) {
+    try {
+        // { id: 'test1', iat: 1705593423, exp: 1705594323, iss: 'EROOM' }
+        return jwt.verify(accessToken, JWT_ACCESS_SECRET);
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            throw new CustomError(error.name, 419, "JWT419", "만료된 토큰입니다.");
+        } else if (error.name === "JsonWebTokenError") {
+            throw new CustomError(error.name, 401, "JWT401", "유효하지 않는 토큰입니다.");
+        } else {
+            throw error;
+        }
+    }
+};
+
+const verifyAccessTokenAndMatchId = (req, res, next) => {
+    const { id } = verifyAccessToken(req.headers['authorization']);
+    if (id === req.body.id) {
+        next();
+    } else {
+        throw new CustomError("Unauthorized", 403, "TC403", "ID에 대한 접근 권한이 없습니다.")
+    }
+}
 
 module.exports = {
     generateAccessToken,
     verifyAccessToken,
     generateRefreshToken,
     refreshAccessToken,
+    verifyAccessTokenAndMatchId,
 };
