82f08a7b397d4af7e741c5333f3ed73014e61a6f
Ksiyeong <philip2767@gmail.com>
2024-01-21 23:56:59 +0900

    Feat: mariadb

---
+++ prac-domain/src/config/database.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const mariadb = require('mariadb/callback');
+
+module.exports = {
+    db: mariadb.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    })
+};
