3e5993afa9c0d6483506d44c70643e3ce422bc40
Ksiyeong <philip2767@gmail.com>
2024-01-22 18:16:55 +0900

    Refactor: createUser() 이메일 중복 검증 로직 추가

--- prac-domain/src/api/user/user.service.js
+++ prac-domain/src/api/user/user.service.js
@@ -1,11 +1,19 @@
 "use strict";
 
+const CustomError = require("../../utils/CustomError");
 const userRepository = require("./user.repository");
 
 module.exports = {
-    createUser: (user) => {
-        userRepository.save(user);
-        return user;
+    createUser: async (user) => {
+        // 사용중인 이메일인지 검증
+        const verifiedUser = await userRepository.findByEmail(user.email)
+        if (verifiedUser) {
+            throw new CustomError('Conflict', 409, 'U409', '이미 사용중인 이메일 입니다.');
+        }
+
+        // 미사용중인 이메일일 경우 유저 생성
+        await userRepository.save(user);
+        return;
     },
 
     findUser: async (email) => {
