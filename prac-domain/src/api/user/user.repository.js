37e4375e604009c47171497557e43a3749198d03
Ksiyeong <philip2767@gmail.com>
2024-01-23 18:36:44 +0900

    Feat: existsByEmail(email) 유저 이메일 존재여부 확인

--- prac-domain/src/api/user/user.repository.js
+++ prac-domain/src/api/user/user.repository.js
@@ -5,10 +5,20 @@ const { db } = require('../../config/database');
 module.exports = {
     findByEmail: (email) => {
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM users WHERE email = ?';
+            const query = 'SELECT * FROM users WHERE email = ?'; //TODO 필요한 데이터만 리턴하도록 변경할 것
             db.query(query, [email], (error, data) => {
                 if (error) reject(`${error}`);
                 else resolve(data[0]);
+            });
+        });
+    },
+
+    existsByEmail: (email) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT COUNT(*) count FROM users WHERE email = ?';
+            db.query(query, [email], (error, data) => {
+                if (error) reject(`${error}`);
+                else resolve(data[0].count); // Bigint
             });
         });
     },
