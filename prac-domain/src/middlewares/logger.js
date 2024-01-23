228aa18542fce16b717c1a49ee493602d75ee451
Ksiyeong <philip2767@gmail.com>
2024-01-23 18:00:24 +0900

    Feat: logger.js 미들웨어 추가

---
+++ prac-domain/src/middlewares/logger.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const log = require('../config/log');
+const CustomError = require('../utils/CustomError');
+
+const logger = (req, res) => {
+    if (req.error) {
+        const err = req.error;
+        if (err instanceof CustomError) { // 400 ~ 499번대 응답
+            log.debug(`${req.method} ${req.path} ${err.status} ${err.code} ${err}`);
+
+        } else { // 500번 이상 응답
+            log.error(`${req.method} ${req.path} 500`, err);
+        }
+
+    } else { // 400번 미만 응답 (정상 응답)
+        log.debug(`${req.method} ${req.path} ${res.statusCode}`);
+    }
+}
+
+module.exports = logger;
