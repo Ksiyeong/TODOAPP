6d9743fcfc8945a626f5d845105ae9a1ae8e056a
Ksiyeong <philip2767@gmail.com>
2024-01-23 18:01:21 +0900

    Feat: 미들웨어 추가 errorHandler, logger

--- prac-domain/src/app.js
+++ prac-domain/src/app.js
@@ -5,17 +5,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
 const router = require('./routes');
-const morgan = require('morgan');
+const errorHandler = require('./middlewares/error-handler');
+const logger = require('./middlewares/logger');
+const log = require('./config/log');
 
 // 미들웨어
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
 
-app.use("/", router);
+app.use('/', router);
+app.use(errorHandler);
+app.use(logger);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
+    log.info(`Server is listening on port ${PORT}`);
+});
