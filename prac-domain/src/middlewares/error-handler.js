aed7dc2bc5049e5fa9b093d3f4bbcbd30fcb39f5
Ksiyeong <philip2767@gmail.com>
2024-01-23 17:59:08 +0900

    Refactor: res.error -> req.error

--- prac-domain/src/middlewares/error-handler.js
+++ prac-domain/src/middlewares/error-handler.js
@@ -3,12 +3,12 @@
 const CustomError = require('../utils/CustomError');
 
 const errorHandler = (err, req, res, next) => {
-    res.error = err; // 에러로그를 남기기 위해 res.error에 발생한 에러를 담아줌
+    req.error = err; // 에러로그를 남기기 위해 req.error에 발생한 에러를 담아줌
     if (err instanceof CustomError) {
         res.status(err.status).json(err.toJSON());
     } else {
         res.status(500).json({
-            name: 'Internal Server Error',
+            name: 'INTERNAL_SERVER_ERROR',
             status: 500,
             code: '500',
             message: '서버 관리자에게 문의하세요.'
