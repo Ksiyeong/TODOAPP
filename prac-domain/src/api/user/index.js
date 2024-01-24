23043878b75afa63444d52717bad333659b0fb42
Ksiyeong <philip2767@gmail.com>
2024-01-24 23:16:12 +0900

    Feat: PATCH /users/:email 회원정보 수정 기능 구현

--- prac-domain/src/api/user/index.js
+++ prac-domain/src/api/user/index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const { Router } = require('express');
-const { getUser, postUser } = require('./user.controller');
+const { getUser, postUser, patchUser } = require('./user.controller');
 
 const users = Router();
 
 users.post('/', postUser);
 users.get('/:email', getUser);
+users.patch('/:email', patchUser);
 
 module.exports = users;
--- prac-domain/src/api/user/user.controller.js
+++ prac-domain/src/api/user/user.controller.js
@@ -27,5 +27,16 @@ module.exports = {
         } catch (error) {
             next(error);
         }
-    }
+    },
+
+    patchUser: async (req, res, next) => {
+        try {
+            req.body.email = req.params.email;
+            const updatedUser = await userService.updateUser(req.body);
+            res.status(200).json(updatedUser);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    },
 };
