6a3828f4ccd8a0723e020a44995a3aa7cc2ed739
Ksiyeong <philip2767@gmail.com>
2024-01-25 23:32:45 +0900

    Feat: /users 인증 추가

--- prac-domain/src/api/user/index.js
+++ prac-domain/src/api/user/index.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const { Router } = require('express');
-const { getUser, postUser, patchUser } = require('./user.controller');
+const { getUser, postUser, patchUser, login } = require('./user.controller');
+const matchAccessTokenAndEmail = require('../../middlewares/matchAccessTokenAndEmail');
 
 const users = Router();
 
 users.post('/', postUser);
-users.get('/:email', getUser);
-users.patch('/:email', patchUser);
+users.post('/login', login); //TODO auth로 따로 뺄까 ?
+users.get('/:email', matchAccessTokenAndEmail, getUser);
+users.patch('/:email', matchAccessTokenAndEmail, patchUser);
 
 module.exports = users;
