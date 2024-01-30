71bdff76b97a6916a77456edbaee38c5b7cbd0a1
Ksiyeong <philip2767@gmail.com>
2024-01-30 18:03:03 +0900

    Refactor: error 자체를 반환하도록 변경

--- prac-domain/src/api/user/user.repository.js
+++ prac-domain/src/api/user/user.repository.js
@@ -7,7 +7,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const query = 'SELECT * FROM users WHERE email = ?'; //TODO 필요한 데이터만 리턴하도록 변경할 것
             db.query(query, [email], (error, data) => {
-                if (error) reject(`${error}`);
+                if (error) reject(error);
                 else resolve(data[0]);
             });
         });
@@ -17,7 +17,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const query = 'SELECT COUNT(*) count FROM users WHERE email = ?';
             db.query(query, [email], (error, data) => {
-                if (error) reject(`${error}`);
+                if (error) reject(error);
                 else resolve(data[0].count); // Bigint
             });
         });
@@ -27,7 +27,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const query = 'INSERT INTO users(email, name, password) VALUES(?, ?, ?);';
             db.query(query, [user.email, user.name, user.password], (error, data) => {
-                if (error) reject(`${error}`);
+                if (error) reject(error);
                 else resolve({
                     affectedRows: data.affectedRows, // int
                     userId: data.insertId // Bigint
@@ -40,7 +40,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const query = 'UPDATE users SET name = ?, password = ? WHERE email = ?'
             db.query(query, [user.name, user.password, user.email], (error, data) => {
-                if (error) reject(`${error}`);
+                if (error) reject(error);
                 else resolve(data.affectedRows); // int
             });
         });
@@ -50,7 +50,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const query = 'DELETE FROM users WHERE email = ?;'
             db.query(query, [email], (error, data) => {
-                if (error) reject(`${error}`);
+                if (error) reject(error);
                 else resolve(data.affectedRows); // int
             });
         });
