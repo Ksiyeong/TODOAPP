d129c4c41f8faf359d7f1841b052944493037be8
Ksiyeong <philip2767@gmail.com>
2024-01-31 10:44:36 +0900

    Refactor: 인증 미들웨어 이름변경

---
+++ prac-domain/src/middlewares/auth-middleware.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const CustomError = require('../utils/CustomError');
+const { verifyAccessToken } = require('../utils/jwtUtils');
+
+module.exports = {
+    matchAccessTokenAndEmail: (req, res, next) => {
+        try {
+            const { email } = verifyAccessToken(req.headers['authorization']);
+            if (email === req.params.email) next();
+            else throw new CustomError('Unauthorized', 403, "TC403", 'ID에 대한 접근 권한이 없습니다.');
+        } catch (error) {
+            next(error);
+        }
+    },
+};
