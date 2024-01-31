9fbb143540de957f2a68275be367cbf35a4daea2
Ksiyeong <philip2767@gmail.com>
2024-01-31 11:11:04 +0900

    Feat: matchAccessTokenAndEmailByBody

--- prac-domain/src/middlewares/auth-middleware.js
+++ prac-domain/src/middlewares/auth-middleware.js
@@ -13,4 +13,14 @@ module.exports = {
             next(error);
         }
     },
+
+    matchAccessTokenAndEmailByBody: (req, res, next) => {
+        try {
+            const { email } = verifyAccessToken(req.headers['authorization']);
+            if (email === req.body.email) next();
+            else throw new CustomError('Unauthorized', 403, "TC403", 'ID에 대한 접근 권한이 없습니다.');
+        } catch (error) {
+            next(error);
+        }
+    },
 };
