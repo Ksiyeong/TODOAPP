bc37f6d0179bd06d66e7cf4b9f1628ca126d8989
Ksiyeong <philip2767@gmail.com>
2024-01-31 11:11:33 +0900

    Feat: POST /posts 게시글 작성 기능 구현

--- prac-domain/src/api/post/index.js
+++ prac-domain/src/api/post/index.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const { Router } = require('express');
-const { getPosts } = require('./post.controller');
+const { getPosts, postPost } = require('./post.controller');
+const { matchAccessTokenAndEmailByBody } = require('../../middlewares/auth-middleware');
 
 const posts = Router();
 
 posts.get('/', getPosts);
+posts.post('/', matchAccessTokenAndEmailByBody, postPost);
 
 module.exports = posts;
--- prac-domain/src/api/post/post.controller.js
+++ prac-domain/src/api/post/post.controller.js
@@ -1,7 +1,32 @@
 "use strict";
 
+const userService = require("../user/user.service");
+const postService = require("./post.service");
+
 module.exports = {
     getPosts: (req, res, next) => {
         res.status(200).json({ message: 'getPosts' });
-    }
+    },
+
+    getPost: (req, res, next) => {
+
+    },
+
+    postPost: async (req, res, next) => {
+        try {
+            const { email, title, content } = req.body; // requestBody에서 데이터 추출
+
+            const { userId } = await userService.findUser(email); // email로 유효한 회원 검색
+            const postId = await postService.createPost(userId, title, content); // post 생성
+
+            res.status(201).json({ postId }); // 응답
+            next();
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    patchPost: (req, res, next) => {
+
+    },
 };
