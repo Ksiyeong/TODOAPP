48ab596291578c69627b9d56f2f0b5450a1f8822
Ksiyeong <philip2767@gmail.com>
2024-02-01 21:49:03 +0900

    Feat: 게시글 단건 조회 라우터 설정

--- prac-domain/src/api/post/index.js
+++ prac-domain/src/api/post/index.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const { Router } = require('express');
-const { getPosts, postPost } = require('./post.controller');
+const { getPosts, postPost, getPost } = require('./post.controller');
 const { matchAccessTokenAndEmailByBody } = require('../../middlewares/auth-middleware');
 
 const posts = Router();
 
-posts.get('/', getPosts);
 posts.post('/', matchAccessTokenAndEmailByBody, postPost);
+posts.get('/:postId', getPost);
+posts.get('/', getPosts);
 
 module.exports = posts;
