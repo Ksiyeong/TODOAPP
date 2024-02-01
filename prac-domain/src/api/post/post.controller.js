ff009abf37a1511e6160a6ead4576a08c549ef3d
Ksiyeong <philip2767@gmail.com>
2024-02-01 21:48:25 +0900

    Feat: GET /posts/:postId 게시글 단건 조회 기능 구현

--- prac-domain/src/api/post/post.controller.js
+++ prac-domain/src/api/post/post.controller.js
@@ -4,14 +4,7 @@ const userService = require("../user/user.service");
 const postService = require("./post.service");
 
 module.exports = {
-    getPosts: (req, res, next) => {
-        res.status(200).json({ message: 'getPosts' });
-    },
-
-    getPost: (req, res, next) => {
-
-    },
-
+    // 게시글 생성
     postPost: async (req, res, next) => {
         try {
             const { email, title, content } = req.body; // requestBody에서 데이터 추출
@@ -26,7 +19,32 @@ module.exports = {
         }
     },
 
-    patchPost: (req, res, next) => {
+    // 단건 조회
+    getPost: async (req, res, next) => {
+        try {
+            const postId = req.params.postId;
+
+            const post = await postService.findPost(postId);
+
+            res.status(200).json(post);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    // 다건 조회
+    getPosts: async (req, res, next) => {
+        res.status(200).json({ message: 'getPosts' });
+    },
+
+    // 게시글 수정
+    patchPost: async (req, res, next) => {
+
+    },
+
+    // 게시글 삭제
+    deletePost: async (req, res, next) => {
 
     },
 };
