9549cf4845c84023bb2263e33a6f44d141f677e8
Ksiyeong <philip2767@gmail.com>
2024-02-12 22:13:52 +0900

    Feat: POST /users 데이터 유효성 검사 추가

--- prac-domain/src/api/user/index.js
+++ prac-domain/src/api/user/index.js
@@ -3,10 +3,23 @@
 const { Router } = require('express');
 const { getUser, postUser, patchUser, login, deleteUser } = require('./user.controller');
 const { matchAccessTokenAndEmailByParams } = require('../../middlewares/auth-middleware');
+const { body } = require('express-validator');
+const validationHandler = require('../../middlewares/validation-handler');
 
 const users = Router();
 
-users.post('/', postUser);
+users.post('/',
+    [
+        body('email')
+            .isEmail().withMessage('이메일 형태로 작성하세요'),
+        body('name')
+            .notEmpty().withMessage('필수')
+            .isLength({ max: 10 }).withMessage('최대10자'),
+        body('password')
+            .isLength({ min: 4 }).withMessage('최소4자'),
+        validationHandler
+    ],
+    postUser);
 users.post('/login', login); //TODO auth로 따로 뺄까 ?
 users.get('/:email', matchAccessTokenAndEmailByParams, getUser);
 users.patch('/:email', matchAccessTokenAndEmailByParams, patchUser);
