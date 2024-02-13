731fb81528f131f7f6194b0bc2cb1e220b5ceb66
Ksiyeong <philip2767@gmail.com>
2024-02-13 23:33:37 +0900

    Feat: app.js module.exports

--- prac-domain/src/app.js
+++ prac-domain/src/app.js
@@ -15,10 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', router);
 app.use(errorHandler);
-app.use(logger);
+app.use(logger); //TODO next()로 받지 않도록 변경해야할듯. winston 또는 morgan 섞는 방식 등등
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     log.info(`Server is listening on port ${PORT}`);
 });
+
+module.exports = app;
