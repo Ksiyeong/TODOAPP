e02e55bd9f2c74a73d59d08407e736a0f0fe1c17
Ksiyeong <philip2767@gmail.com>
2024-02-14 23:57:06 +0900

    Test: POST /users 데이터 유효성 검사 분리

--- prac-domain/src/tests/unit/api/user/user.controller.test.js
+++ prac-domain/src/tests/unit/api/user/user.controller.test.js
@@ -2,125 +2,128 @@
 
 const request = require('supertest');
 const app = require('../../../../app');
-const userService = require('../../../../api/user/user.service');
-const CustomError = require('../../../../utils/CustomError');
-const jwtUtils = require('../../../../utils/jwtUtils');
 const userController = require('../../../../api/user/user.controller');
+const userService = require('../../../../api/user/user.service');
+const jwtUtils = require('../../../../utils/jwtUtils');
 
 describe('POST /users', () => {
-    it('회원가입 성공', async () => {
-        // given
-        const requestBody = {
-            email: 'test3@test.com',
-            name: '테스트3',
-            password: '1234'
-        };
+    describe('postUser', () => {
+        let req, res, next;
+        beforeEach(() => {
+            req = {
+                body: {
+                    email: 'test@test.com',
+                    name: '테스트',
+                    password: '1234'
+                }
+            };
+            res = {
+                status: jest.fn(() => res),
+                json: jest.fn()
+            };
+            next = jest.fn();
+        });
 
-        const createdUser = {
-            email: requestBody.email,
-            name: requestBody.name
-        };
-        userService.createUser = jest.fn().mockResolvedValue(createdUser);
+        it('회원가입 성공하여 정상 응답', async () => {
+            // given
+            const createdUser = {
+                email: req.body.email,
+                name: req.body.name
+            };
+            userService.createUser = jest.fn().mockResolvedValue(createdUser);
 
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
+            // when
+            await userController.postUser(req, res, next);
 
-        // then
-        expect(res.status).toBe(201);
-        expect(res.body).toEqual(createdUser);
-        expect(userService.createUser).toHaveBeenCalledWith(requestBody);
+            // then
+            expect(userService.createUser).toBeCalledWith(req.body);
+            expect(res.status).toBeCalledWith(201);
+            expect(res.json).toBeCalledWith(createdUser);
+        });
+
+        it('회원가입 중 에러발생하여 에러핸들러로 넘김', async () => {
+            // given
+            const error = new Error('에러 발생');
+            userService.createUser = jest.fn().mockRejectedValue(error);
+
+            // when
+            await userController.postUser(req, res, next);
+
+            // then
+            expect(userService.createUser).toBeCalledWith(req.body);
+            expect(next).toBeCalledWith(error);
+        });
     });
 
-    it('회원가입 실패 - email 중복', async () => {
-        // given
-        const requestBody = {
-            email: 'test3@test.com',
-            name: '테스트3',
-            password: '1234'
-        };
+    describe('데이터유효성검사', () => {
+        it('회원가입 실패 - 잘못된 email 형식', async () => {
+            // given
+            const requestBody = {
+                email: 'test3test.com',
+                name: '테스트3',
+                password: '1234'
+            };
 
-        userService.createUser = jest.fn().mockRejectedValue(new CustomError('CONFLICT', 409, 'U409', '이미 사용중인 이메일 입니다.'));
+            // when
+            const res = await request(app)
+                .post('/users')
+                .send(requestBody);
 
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
+            // then
+            expect(res.status).toBe(400);
+        });
 
-        // then
-        expect(res.status).toBe(409);
-        expect(userService.createUser).toHaveBeenCalledWith(requestBody);
+        it('회원가입 실패 - 잘못된 name 형식(empty)', async () => {
+            // given
+            const requestBody = {
+                email: 'test3@test.com',
+                // name: '테스트3',
+                password: '1234'
+            };
+
+            // when
+            const res = await request(app)
+                .post('/users')
+                .send(requestBody);
+
+            // then
+            expect(res.status).toBe(400);
+        });
+
+        it('회원가입 실패 - 잘못된 name 형식(10자 초과)', async () => {
+            // given
+            const requestBody = {
+                email: 'test3@test.com',
+                name: '테스트테스트테스트33',
+                password: '1234'
+            };
+
+            // when
+            const res = await request(app)
+                .post('/users')
+                .send(requestBody);
+
+            // then
+            expect(res.status).toBe(400);
+        });
+
+        it('회원가입 실패 - 잘못된 password 형식(4자 미만)', async () => {
+            // given
+            const requestBody = {
+                email: 'test3@test.com',
+                name: '테스트3',
+                password: '123'
+            };
+
+            // when
+            const res = await request(app)
+                .post('/users')
+                .send(requestBody);
+
+            // then
+            expect(res.status).toBe(400);
+        });
     });
-
-    it('회원가입 실패 - 잘못된 email 형식', async () => {
-        // given
-        const requestBody = {
-            email: 'test3test.com',
-            name: '테스트3',
-            password: '1234'
-        };
-
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
-
-        // then
-        expect(res.status).toBe(400);
-    });
-
-    it('회원가입 실패 - 잘못된 name 형식(empty)', async () => {
-        // given
-        const requestBody = {
-            email: 'test3@test.com',
-            // name: '테스트3',
-            password: '1234'
-        };
-
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
-
-        // then
-        expect(res.status).toBe(400);
-    });
-
-    it('회원가입 실패 - 잘못된 name 형식(10자 초과)', async () => {
-        // given
-        const requestBody = {
-            email: 'test3@test.com',
-            name: '테스트테스트테스트33',
-            password: '1234'
-        };
-
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
-
-        // then
-        expect(res.status).toBe(400);
-    });
-
-    it('회원가입 실패 - 잘못된 password 형식(4자 미만)', async () => {
-        // given
-        const requestBody = {
-            email: 'test3@test.com',
-            name: '테스트3',
-            password: '123'
-        };
-
-        // when
-        const res = await request(app)
-            .post('/users')
-            .send(requestBody);
-
-        // then
-        expect(res.status).toBe(400);
-    });
-
 });
 
 describe('POST /users/login', () => {
