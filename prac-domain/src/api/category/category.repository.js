c2f029380791cf6ffa3a0949eb622fac615abded
Ksiyeong <philip2767@gmail.com>
2024-02-17 01:08:15 +0900

    Feat: category.repository.save 카테고리 저장 기능 구현

--- prac-domain/src/api/category/category.repository.js
+++ prac-domain/src/api/category/category.repository.js
@@ -5,10 +5,23 @@ const { db } = require("../../config/database");
 module.exports = {
     existsByName: (name) => {
         return new Promise((resolve, reject) => {
-            const query = 'SELECT EXISTS (SELECT 1 FROM category WHERE name = ?) AS count';
+            const query = 'SELECT EXISTS (SELECT 1 FROM category WHERE name = ?) AS count;';
             db.query(query, [name], (error, data) => {
                 if (error) reject(error);
                 else resolve(data[0].count); // number
+            });
+        });
+    },
+
+    save: (name) => {
+        return new Promise((resolve, reject) => {
+            const query = 'INSERT INTO category(name) VALUES(?);';
+            db.query(query, [name], (error, data) => {
+                if (error) reject(error);
+                else resolve({
+                    affectedRows: data.affectedRows, // int
+                    categoryId: data.insertId // Bigint
+                });
             });
         });
     },
