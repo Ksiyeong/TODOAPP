bab5df27c9daa347eb7c369c718d5d717214b675
Ksiyeong <philip2767@gmail.com>
2024-02-17 23:33:25 +0900

    Chore: log삭제

--- prac-domain/src/api/category/category.controller.js
+++ prac-domain/src/api/category/category.controller.js
@@ -5,9 +5,7 @@ const categoryService = require("./category.service");
 module.exports = {
     postCategory: async (req, res, next) => {
         try {
-            console.log('안뇽1');
             const response = await categoryService.createCategory(req.body.name);
-            console.log('안뇽2');
             res.status(201).json(response);
             next();
         } catch (error) {
